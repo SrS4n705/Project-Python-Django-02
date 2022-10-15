345716ca072fa3ad8bbe31e5acc7792c9fd40428
Joelio Santos <joeliosantos70x7@gmail.com>
2022-10-15 10:10:00 -0300

    atualização 2

--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Joelio Santos'})
+        'name': 'Luiz Otávio',
+    })
