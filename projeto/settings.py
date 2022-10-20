1968478198e82eafeb0f5415f5413b0f9908b29f
Joelio Santos <joeliosantos70x7@gmail.com>
2022-10-20 20:00:18 -0300

    atualização

--- projeto/settings.py
+++ projeto/settings.py
@@ -121,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'recipes/static/'
+
+STATIC_DIRS = [
+    BASE_DIR / ''
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
