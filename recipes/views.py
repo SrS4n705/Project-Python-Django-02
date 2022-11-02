51e522b65ef05cf58f368939a88a3b6ba4fdf4d4
Joelio Santos <joeliosantos70x7@gmail.com>
2022-11-02 01:31:04 -0300

    Atualização 0130

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
@@ -33,4 +35,24 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
+    })
+
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    )
+    recipes = recipes.order_by('-id')
+    recipes = recipes.filter(is_published=True)
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" | ',
+        'search_term': search_term,
+        'recipes': recipes,
     })
