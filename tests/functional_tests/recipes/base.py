375b8d70c21a014dff6fd5da0dbfed208d4afce8
Joelio Santos <joeliosantos70x7@gmail.com>
2022-11-21 15:17:33 -0300

    atualização 1121

--- tests/functional_tests/recipes/base.py
+++ tests/functional_tests/recipes/base.py
@@ -1,13 +1,12 @@
 import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from utils.browser import make_chrome_browser
 
 from recipes.tests.test_recipe_base import RecipeMixin
-from utils.browser import make_chrome_browser
 
 
 class RecipeBaseFunctionalTest(StaticLiveServerTestCase, RecipeMixin):
-
     def setUp(self) -> None:
         self.browser = make_chrome_browser()
         return super().setUp()
